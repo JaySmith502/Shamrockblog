54fd9ddf69ad63377fe8792c6d2e33f1f928311b
Jason E Smith <smith1jason@yahoo.com>
2015-11-14 20:19:58 -0500

    Update error handler
    instead of echo, wrapped error message in $error array so it would style
    as others.

--- Shamrockblog/admin/add_user.php
+++ Shamrockblog/admin/add_user.php
@@ -50,7 +50,7 @@ if (isset($_POST['submit'])) {
         $sthandler->execute();
 
         if($sthandler->rowCount() > 0){
-        echo "Username already exists, please choose another.";
+        $error[] = "Username already exists, please choose another.";
         }  else {
         try {
 
